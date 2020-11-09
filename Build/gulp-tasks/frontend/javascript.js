7c39e0ee2774531b742cd4869336cb57db6ef634
Volker Kemeter <v.kemeter@supseven.at>
2020-11-09 14:54:51 +0100

    [TASK] removed sourcemap from typescript
    - not really needed.

--- Build/gulp-tasks/frontend/javascript.js
+++ Build/gulp-tasks/frontend/javascript.js
@@ -1,7 +1,6 @@
 var _gulp = require('gulp'),
     _rename = require('gulp-rename'),
     _uglify = require('gulp-uglify'),
-    _sourcemaps = require('gulp-sourcemaps'),
     _ts = require('gulp-typescript'),
     _config = require('../../config.js');
 
@@ -18,10 +17,8 @@ module.exports = function() {
         .pipe(_ts({
             outFile: _config().frontend.javascript.outFile
         }))
-        .pipe(_sourcemaps.init())
         .pipe(_rename({ suffix: '.min' }))
         .pipe(_uglify())
-        .pipe(_sourcemaps.write('.'))
         .pipe(_gulp.dest(_config().frontend.javascript.dest));
 };
 
