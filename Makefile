c932e7b32a9cbb7faa1947aab4a90a73692f2f4d
Georg Großberger <contact@grossberger-ge.org>
2021-02-17 14:53:41 +0100

    [TASK] Add build helpers

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 
 .PHONY: build
-build: vendor/autoload.php
+build: vendor/autoload.php Build/node_modules/.yarn-integrity
+
+frontend: Build/node_modules/.yarn-integrity
+	@cd Build && yarn gulp
 
 fix: vendor/autoload.php
-	@bin/php-cs-fixer fix --config=.php_cs --diff -vvv
+	@bin/php-cs-fixer fix --config=.php_cs --diff -vvv --diff-format=udiff
 
 lint: vendor/autoload.php
-	@bin/php-cs-fixer fix --config=.php_cs --diff -vvv --dry-run
+	@bin/php-cs-fixer fix --config=.php_cs --diff -vvv --dry-run --diff-format=udiff
 
 test: vendor/autoload.php
 	@bin/phpunit -c phpunit.xml
@@ -20,4 +23,7 @@ clobber: clean
 	@rm -rf vendor bin
 
 vendor/autoload.php:
-	@composer install
+	@composer install --ignore-platform-reqs --no-plugins --no-scripts && touch vendor/autoload.php
+
+Build/node_modules/.yarn-integrity:
+	@cd Build && yarn install --frozen-lockfile --prefer-offline && touch node_modules/.yarn-integrity
--- test-all.sh
+++ test-all.sh
@@ -1,43 +1,42 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0 && pwd)
+BASE_DIR="$(cd $(dirname $0) && pwd)"
+cd "${BASE_DIR}"
 
 set -e -u
 
-PHPs='7.2
-7.3
-7.4';
+PHPs='7.2-node10
+7.3-node12
+7.4-node14';
 
-TYPO3s='typo3/cms-core:^8.7 typo3/cms-extbase:^8.7 typo3/cms-fluid:^8.7 typo3/cms-frontend:^8.7
-typo3/cms-core:^9.5 typo3/cms-extbase:^9.5 typo3/cms-fluid:^9.5 typo3/cms-frontend:^9.5
-typo3/cms-core:^10 typo3/cms-extbase:^10 typo3/cms-fluid:^10 typo3/cms-frontend:^10'
+TYPO3s='9.5
+10.4'
 
 C_HOME="$(composer global config home -q)"
 C_CACHE="$(composer global config cache-dir -q)"
-BASE_DIR="$(cd $(dirname $0) && pwd)"
 
 cd ${BASE_DIR}
 
 single_docker_cmd() {
     docker run --rm -it \
+        -u $(id -u):$(id -g) \
         -v "${C_HOME}:/var/lib/composer" -e COMPOSER_HOME=/var/lib/composer \
         -v "${C_CACHE}:/var/cache/composer" -e COMPOSER_CACHE_DIR=/var/cache/composer \
-        -v ${BASE_DIR}:/opt/build -w /opt/build \
-        garfieldius/typo3-ci:php${1}-node12 sh -c "${2}"
+        -v ${BASE_DIR}:/build -w /build \
+        garfieldius/typo3-ci:php${1} bash -e -c "${2}"
 }
 
 run_test() {
     echo "Testing ${1} with PHP ${2}"
     git checkout -- composer.json
-    cat composer.json | jq 'del(.require)' > composer.json
-    rm -rf composer.lock vendor bin typo3_src typo3 index.php
+    jq 'del(.require)' composer.json | jq 'del(.["require-dev"])' > c.json
+    rm -rf composer.lock vendor bin typo3_src typo3 index.php composer.json
+    mv c.json composer.json
 
-    single_docker_cmd "${2}" "composer require --ansi -n --no-suggest --no-progress -vv --no-plugins --no-scripts ${1}"
+    single_docker_cmd "${2}" "composer require --ansi -n --no-progress --no-plugins --no-scripts 'typo3/cms-core:^${1}' 'typo3/cms-extbase:^${1}' 'typo3/cms-fluid:^${1}' 'typo3/cms-frontend:^${1}'"
+    single_docker_cmd "${2}" "composer require --dev --ansi -n --no-progress --no-plugins --no-scripts phpunit/phpunit"
     single_docker_cmd "${2}" 'php ./bin/phpunit -c phpunit.xml'
 }
-
-# Only PHP for v7
-run_test "typo3/cms:^7.6" "7.2"
 
 # Full matrix for newer
 for t3 in ${TYPO3s}; do
@@ -45,6 +44,3 @@ for t3 in ${TYPO3s}; do
         run_test "${t3}" "${php}"
     done
 done
-
-git checkout -- composer.json composer.lock
-rm -rf composer.lock vendor bin typo3_src typo3 index.php
