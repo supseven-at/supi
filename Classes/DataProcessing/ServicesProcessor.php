06f34158a9dfcdc7e70992071b5b29d12ae120f0
Georg Großberger <g.grossberger@supseven.at>
2022-06-24 11:04:39 +0200

    [TASK] Add data processor to convert services attr list to JSON

---
+++ Classes/DataProcessing/ServicesProcessor.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Supseven\Supi\DataProcessing;
+
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+
+/**
+ * @author Georg Großberger <g.grossberger@supseven.at>
+ */
+class ServicesProcessor implements DataProcessorInterface
+{
+    public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData)
+    {
+        foreach ($processedData['settings']['services'] ?? [] as $key => $data) {
+            if (!empty($data['attr'])) {
+                $processedData['settings']['services'][$key]['attrJson'] = json_encode($data['attr']);
+            }
+        }
+
+        return $processedData;
+    }
+}
---
+++ Tests/DataProcessing/ServicesProcessorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Supseven\Supi\Tests\DataProcessing;
+
+use PHPUnit\Framework\TestCase;
+use Supseven\Supi\DataProcessing\ServicesProcessor;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+
+/**
+ * @author Georg Großberger <g.grossberger@supseven.at>
+ */
+class ServicesProcessorTest extends TestCase
+{
+    public function testProcess()
+    {
+        $cObj = $this->createMock(ContentObjectRenderer::class);
+
+        $attr = [
+            'src'    => 'https://www.service.com/embed',
+            'height' => '1000',
+        ];
+        $in = [
+            'settings' => [
+                'services' => [
+                    'sage' => [
+                        'label'   => 'My Service',
+                        'service' => 'service',
+                        'type'    => 'iframe',
+                        'attr'    => $attr,
+                    ],
+                ],
+            ],
+        ];
+
+        $expected = $in;
+        $expected['settings']['services']['sage']['attrJson'] = json_encode($attr);
+
+        $subject = new ServicesProcessor();
+        $actual = $subject->process($cObj, [], [], $in);
+
+        self::assertEquals($expected, $actual);
+    }
+}
