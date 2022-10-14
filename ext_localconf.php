04ee7d506f8e3a5e132e0f9209227589958b0f70
Helmut Strasser <h.strasser@supseven.at>
2022-10-14 10:30:09 +0200

    [TASK] Move supi icon registration from tt_content.php to ext_localconf.php.
    The registration of the supi icon is not working if the $iconRegistry->registerIcon() method is called in the TCA override file, it is only working in the ext_localconf (up to v11.4).

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,18 @@
 <?php
 
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:supi/Configuration/TSConfig/Page/NewElementWizard.tsconfig">');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:supi/Configuration/TSConfig/Page/YoutubeElement.tsconfig">');
+
+// Icon Registry
+$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+$iconRegistry->registerIcon(
+    'supi',
+    SvgIconProvider::class,
+    [
+        'source' => 'EXT:supi/Resources/Public/Icons/Extension.svg'
+    ]
+);
