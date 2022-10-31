697f81a5a41d1d41bbfb305fa9a8cf6ec02824ef
Helmut Strasser <h.strasser@supseven.at>
2022-10-31 14:48:32 +0100

    [TASK] Constrain icon registration to TYPO3 version < 11.5.0.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,16 @@
 <?php
 
-use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
-use TYPO3\CMS\Core\Imaging\IconRegistry;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:supi/Configuration/TSConfig/Page/NewElementWizard.tsconfig">');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:supi/Configuration/TSConfig/Page/YoutubeElement.tsconfig">');
 
 // Icon Registry
-$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-$iconRegistry->registerIcon(
-    'supi',
-    SvgIconProvider::class,
-    [
-        'source' => 'EXT:supi/Resources/Public/Icons/Extension.svg'
-    ]
-);
+if (version_compare((string)\TYPO3\CMS\Core\Utility\VersionNumberUtility::getCurrentTypo3Version(), '11.5.0') < 0) {
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'supi',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        [
+            'source' => 'EXT:supi/Resources/Public/Icons/Extension.svg'
+        ]
+    );
+}
