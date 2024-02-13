15caf9c086b130284b58f43faf630f9aa85303d0
Georg Großberger <g.grossberger@supseven.at>
2024-02-13 10:43:02 +0100

    [BUGFIX] Save CSP hashes in KV store
    Needed to send them on cached pages

--- Classes/CSP/SupiPolicyExtender.php
+++ Classes/CSP/SupiPolicyExtender.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Supseven\Supi\CSP;
 
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Event\PolicyMutatedEvent;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\HashValue;
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 /**
  * Registry and event listener to extend the CSP config
@@ -16,18 +18,47 @@ use TYPO3\CMS\Core\SingletonInterface;
  */
 class SupiPolicyExtender implements SingletonInterface
 {
-    private array $extensions = [];
+    public function __construct(
+        private readonly FrontendInterface $cache,
+    ) {
+    }
 
     public function __invoke(PolicyMutatedEvent $event): void
     {
-        foreach ($this->extensions as $extension) {
-            $event->getCurrentPolicy()->extend(...$extension);
+        if (empty($GLOBALS['TSFE'])) {
+            return;
+        }
+
+        $hashes = $this->cache->get($this->getCacheKey()) ?: [];
+
+        foreach ($hashes as $hash) {
+            $event->getCurrentPolicy()->extend(Directive::ScriptSrc, new HashValue($hash));
         }
     }
 
     public function addInlineScript(string $data): void
     {
         $hash = hash('sha256', $data);
-        $this->extensions[] = [Directive::ScriptSrc, new HashValue($hash)];
+        $hashes = $this->cache->get($this->getCacheKey());
+
+        if (!is_array($hashes)) {
+            $hashes = [];
+        }
+
+        if (!in_array($hash, $hashes)) {
+            $hashes[] = $hash;
+
+            $this->cache->set($this->getCacheKey(), $hashes, ['pageId_' . $this->getTSFE()->id], 31536000);
+        }
+    }
+
+    private function getCacheKey(): string
+    {
+        return 'supi_hashes_' . $this->getTSFE()->newHash;
+    }
+
+    private function getTSFE(): TypoScriptFrontendController
+    {
+        return $GLOBALS['TSFE'];
     }
 }
