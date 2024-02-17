3a36947a8ad5af1e42323957f086cba5eb51a64f
Helmut Strasser <h.strasser@supseven.at>
2024-02-17 21:41:59 +0100

    [TASK] Add missing cache mocking

--- Tests/CSP/SupiPolicyExtenderTest.php
+++ Tests/CSP/SupiPolicyExtenderTest.php
@@ -6,11 +6,13 @@ namespace Supseven\Supi\Tests\CSP;
 
 use PHPUnit\Framework\TestCase;
 use Supseven\Supi\CSP\SupiPolicyExtender;
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Event\PolicyMutatedEvent;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\HashValue;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Policy;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Scope;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 /**
  * @author Georg Großberger <g.grossberger@supseven.at>
@@ -20,14 +22,16 @@ class SupiPolicyExtenderTest extends TestCase
     public function testAddInlineScript(): void
     {
         $data = 'console.log("I am a Javascript");';
-        $hash = new HashValue(hash('sha256', $data));
+        $hash = hash('sha256', $data);
+        $hashValue = new HashValue($hash);
+        $newHash = 'abc';
         $directive = Directive::ScriptSrc;
 
         $policy = $this->createMock(Policy::class);
         $policy
             ->expects($this->once())
             ->method('extend')
-            ->with($this->equalTo($directive), $this->equalTo($hash));
+            ->with($this->equalTo($directive), $this->equalTo($hashValue));
 
         $event = new PolicyMutatedEvent(
             Scope::frontend(),
@@ -35,7 +39,19 @@ class SupiPolicyExtenderTest extends TestCase
             $policy
         );
 
-        $subject = new SupiPolicyExtender();
+        $cache = $this->createMock(FrontendInterface::class);
+        $cache
+            ->expects($this->exactly(2))
+            ->method('get')
+            ->with($this->equalTo('supi_hashes_' . $newHash))
+            ->willReturn([$hash]);
+
+        $tsfe = $this->createMock(TypoScriptFrontendController::class);
+        $tsfe->id = 123;
+        $tsfe->newHash = $newHash;
+        $GLOBALS['TSFE'] = $tsfe;
+
+        $subject = new SupiPolicyExtender($cache);
         $subject->addInlineScript($data);
         $subject($event);
     }
