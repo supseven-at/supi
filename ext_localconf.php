e5f284349a04444c87acb0bf20cca93a2b91fa4b
Georg Großberger <g.grossberger@supseven.at>
2025-09-23 11:54:14 +0200

    [TASK] Remove obsolte TSConfig includes

--- ext_localconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:supi/Configuration/TSConfig/Page/NewElementWizard.tsconfig">');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:supi/Configuration/TSConfig/Page/YoutubeElement.tsconfig">');
