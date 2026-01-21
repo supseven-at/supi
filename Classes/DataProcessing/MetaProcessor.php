9153350423570e1a7a84784f314fd3ea223f18ef
Volker Kemeter <v.kemeter@supseven.at>
2026-01-21 12:23:26 +0100

    [TASK] allow reels in meta processor

--- Classes/DataProcessing/MetaProcessor.php
+++ Classes/DataProcessing/MetaProcessor.php
@@ -13,12 +13,12 @@ class MetaProcessor implements DataProcessorInterface
     {
         $allowedUrls = [
             'https://www.instagram.com/p/',
+            'https://www.instagram.com/reel/',
         ];
 
         $posts = preg_split('/\\R/', $cObj->data['bodytext']);
 
         $filteredUrls = array_values(array_filter($posts, function (string $url) use ($allowedUrls): bool {
-            $url = trim($url);
             foreach ($allowedUrls as $allowed) {
                 if (str_starts_with($url, $allowed)) {
                     return true;
@@ -32,7 +32,6 @@ class MetaProcessor implements DataProcessorInterface
             return $processedData;
         }
 
-        $id = $cObj->data['bodytext'];
         $as = $processorConfiguration['as'] ?? 'metaAttr';
         $vals = $processedData['settings']['meta']['iframeAttr'] ?? [];
         $vals['src'] = '//www.instagram.com/embed.js';
